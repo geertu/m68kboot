a5e05515fff9a1ef1b1954d07e50159d8a5ab86f
rnhodek <rnhodek>
1997-08-23 20:36:19 +0000

    Deleted obsolete prototype of CheckVectorDevice

--- lilo/atari/lilo.h
+++ lilo/atari/lilo.h
@@ -6,10 +6,13 @@
  *  This file is subject to the terms and conditions of the GNU General Public
  *  License.  See the file COPYING for more details.
  * 
- * $Id: lilo.h,v 1.1 1997-08-12 15:27:09 rnhodek Exp $
+ * $Id: lilo.h,v 1.2 1997-08-23 20:36:19 rnhodek Exp $
  * 
  * $Log: lilo.h,v $
- * Revision 1.1  1997-08-12 15:27:09  rnhodek
+ * Revision 1.2  1997-08-23 20:36:19  rnhodek
+ * Deleted obsolete prototype of CheckVectorDevice
+ *
+ * Revision 1.1  1997/08/12 15:27:09  rnhodek
  * Import of Amiga and newly written Atari lilo sources, with many mods
  * to separate out common parts.
  *
@@ -32,7 +35,6 @@ enum flopok {
 
 void parse_device( const char *device, int *devnum, unsigned long *start,
                    int allow_xgm, enum flopok floppy_ok );
-void CheckVectorDevice( struct vecent *vector, dev_t device );
 int main( int argc, char *argv[] );
 
 /************************* End of Prototypes **************************/
