ff92a6d49455b16e5da3ab32b1f2785d805b86d4
rnhodek <rnhodek>
1997-08-23 22:43:44 +0000

    Removed leftovers from ancient versions

--- lilo/atari/bootparam.h
+++ lilo/atari/bootparam.h
@@ -7,13 +7,15 @@
  * published by the Free Software Foundation: either version 2 or
  * (at your option) any later version.
  * 
- * $Id: bootparam.h,v 1.1 1997-08-12 15:27:08 rnhodek Exp $
+ * $Id: bootparam.h,v 1.2 1997-08-23 22:43:44 rnhodek Exp $
  * 
  * $Log: bootparam.h,v $
- * Revision 1.1  1997-08-12 15:27:08  rnhodek
+ * Revision 1.2  1997-08-23 22:43:44  rnhodek
+ * Removed leftovers from ancient versions
+ *
+ * Revision 1.1  1997/08/12 15:27:08  rnhodek
  * Import of Amiga and newly written Atari lilo sources, with many mods
  * to separate out common parts.
- *
  * 
  */
 
@@ -76,71 +78,5 @@ typedef struct {
 #define	BP_FLAGS		40
 #define	BP_ENV			44
 #define	BP_CMDLIN		128
-
-/*
- * layout of loader
- * ----------------
- */
-
-/* unions are used to force block sizes */
-
-#define	N_MAPENTRIES	85
-#define	MAX_TMPMOUNT	4
-
-#ifndef __ASSEMBLY__
-
-typedef struct {
-	/* root sector */
-	union {
-		char length[512];
-		struct {
-			u_short	branch;
-			char	biosparam[34];
-			u_long	lilo_magic;
-			short	bootdev;
-#ifdef SECONDARY_ROOTSEC_NEEDED
-			u_long	next_sector;
-#endif
-			u_long	map_sector;
-		} rootsec_s __attribute((packed));
-	} rootsec_u;
-#ifdef SECONDARY_ROOTSEC_NEEDED
-	/* Space for a second root sector, to extend the first. This is currently
-	 * not needed, all code fits into the master root sector. */
-	char secondary_rootsec[512];
-#endif /* SECONDARY_ROOTSEC_NEEDED */
-	/* map sector */
-	union {
-		char length[512];
-		struct maprec map_entries[N_MAPENTRIES];
-	} mapblock_u;
-	/* start of TOS program */
-	struct {
-		TOS_PRG_HDR header;
-		u_short	branch;
-		u_short	timeout;		/* 5 msec delay until input time-out,
-								 * 0xffff: never */
-		u_short	delay;			/* delay: wait that many 5 msec units. */
-		u_short	port;			/* serial port. 0 = none, 1 = Modem1, etc. */
-		u_long	ser_param;		/* RS-232 parameters (low word: speed, high
-								 * word: ucr for Rsconf()) */
-		SECTOR_ADDR descr[3];
-		u_short	prompt;			/* 0 = only on demand, =! 0 = always */
-		u_short	msg_len;		/* 0 if none */
-		SECTOR_ADDR msg;		/* message sector */
-		u_short tmpmount[MAX_TMPMOUNT];	/* temp mount descriptors */
-		
-		
-	} tosprg  __attribute((packed));
-} BOOT_SECTOR  __attribute((packed));
-
-#define	lilo_magic		rootsec_u.rootsec_s.lilo_magic
-#define	bootdev			rootsec_u.rootsec_s.bootdev
-#define	next_sector		rootsec_u.rootsec_s.next_sector
-#define	map_sector		rootsec_u.rootsec_s.map_sector
-
-#define map_entries		mapblock_u.map_entries
-
-#endif /* __ASSEMBLY__ */
 
 #endif  /* _bootparm_h */
