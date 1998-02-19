999aed99a2114b98ffdc9e633d758e440e4caf61
rnhodek <rnhodek>
1998-02-19 21:07:08 +0000

    Added if/else braces to avoid gcc warnings

--- lilo/amiga/lilo.l.c
+++ lilo/amiga/lilo.l.c
@@ -13,10 +13,13 @@
  *  This file is subject to the terms and conditions of the GNU General Public
  *  License.  See the file COPYING for more details.
  * 
- * $Id: lilo.l.c,v 1.2 1997-08-12 21:51:05 rnhodek Exp $
+ * $Id: lilo.l.c,v 1.3 1998-02-19 21:07:08 rnhodek Exp $
  * 
  * $Log: lilo.l.c,v $
- * Revision 1.2  1997-08-12 21:51:05  rnhodek
+ * Revision 1.3  1998-02-19 21:07:08  rnhodek
+ * Added if/else braces to avoid gcc warnings
+ *
+ * Revision 1.2  1997/08/12 21:51:05  rnhodek
  * Written last missing parts of Atari lilo and made everything compile
  *
  * Revision 1.1  1997/08/12 15:27:04  rnhodek
@@ -426,42 +429,48 @@ int main(int argc, char *argv[])
 	    DoCreateReset = 1;
 	else if (!strcmp(argv[0], "-V") || !strcmp(argv[0], "--version"))
 	    Die(LiloVersion);
-	else if (!strcmp(argv[0], "-w") || !strcmp(argv[0], "--root"))
+	else if (!strcmp(argv[0], "-w") || !strcmp(argv[0], "--root")) {
 	    if (argc-- > 1 && !Root) {
 		Root = argv[1];
 		argv++;
 	    } else
 		Usage();
-	else if (!strcmp(argv[0], "-r") || !strcmp(argv[0], "--restore-from"))
+	}
+	else if (!strcmp(argv[0], "-r") || !strcmp(argv[0], "--restore-from")){
 	    if (argc-- > 1 && !RestoreBootBlock) {
 		RestoreBootBlock = argv[1];
 		argv++;
 	    } else
 		Usage();
-	else if (!strcmp(argv[0], "-s") || !strcmp(argv[0], "--save-to"))
+	}
+	else if (!strcmp(argv[0], "-s") || !strcmp(argv[0], "--save-to")) {
 	    if (argc-- > 1 && !SaveBootBlock) {
 		SaveBootBlock = argv[1];
 		argv++;
 	    } else
 		Usage();
-	else if (!strcmp(argv[0], "-d") || !strcmp(argv[0], "--device"))
+	}
+	else if (!strcmp(argv[0], "-d") || !strcmp(argv[0], "--device")) {
 	    if (argc-- > 1 && !device) {
 		device = argv[1];
 		argv++;
 	    } else
 		Usage();
-	else if (!strcmp(argv[0], "-t") || !strcmp(argv[0], "--dostype"))
+	}
+	else if (!strcmp(argv[0], "-t") || !strcmp(argv[0], "--dostype")) {
 	    if (argc-- > 1 && !DosType) {
 		DosType = argv[1];
 		argv++;
 	    } else
 		Usage();
-	else if (!strcmp(argv[0], "-C") || !strcmp(argv[0], "--config-file"))
+	}
+	else if (!strcmp(argv[0], "-C") || !strcmp(argv[0], "--config-file")) {
 	    if (argc-- > 1 && !conffile) {
 		conffile = argv[1];
 		argv++;
 	    } else
 		Usage();
+	}
 	else
 	    Usage();
     }
