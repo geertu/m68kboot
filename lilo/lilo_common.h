065db62a1e1a934f7b785972f189ad5d780e7b2e
rnhodek <rnhodek>
1998-02-23 10:15:21 +0000

    Update copyright years in banner message.

--- lilo/lilo_common.h
+++ lilo/lilo_common.h
@@ -7,10 +7,13 @@
  *  This file is subject to the terms and conditions of the GNU General Public
  *  License.  See the file COPYING for more details.
  * 
- * $Id: lilo_common.h,v 1.2 1997-09-19 09:06:47 geert Exp $
+ * $Id: lilo_common.h,v 1.3 1998-02-23 10:15:21 rnhodek Exp $
  * 
  * $Log: lilo_common.h,v $
- * Revision 1.2  1997-09-19 09:06:47  geert
+ * Revision 1.3  1998-02-23 10:15:21  rnhodek
+ * Update copyright years in banner message.
+ *
+ * Revision 1.2  1997/09/19 09:06:47  geert
  * Big bunch of changes by Geert: make things work on Amiga; cosmetic things
  *
  * Revision 1.1  1997/08/12 15:26:56  rnhodek
@@ -26,7 +29,7 @@
 #include "version.h"
 
 #define LILO_VERSION "Linux/m68k Loader version " VERSION WITH_BOOTP "\n" \
-					 "(C) Copyright 1995-1997 by Geert Uytterhoeven, Roman Hodek\n"
+					 "(C) Copyright 1995-1998 by Geert Uytterhoeven, Roman Hodek\n"
 
 extern const char LiloVersion[];
 
