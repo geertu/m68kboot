81e79d908de53d04042d00c7bdcb63c7d42c9a10
rnhodek <rnhodek>
1998-04-07 09:43:00 +0000

    *** empty log message ***

---
+++ lilo/atari/strlib.h
@@ -0,0 +1,23 @@
+/* strlib.h -- 
+ *
+ * Copyright (C) 1998 Roman Hodek <Roman.Hodek@informatik.uni-erlangen.de>
+ *
+ * This program is free software.  You can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation: either version 2 or
+ * (at your option) any later version.
+ * 
+ * $Id: strlib.h,v 1.1 1998-04-07 09:43:00 rnhodek Exp $
+ * 
+ * $Log: strlib.h,v $
+ * Revision 1.1  1998-04-07 09:43:00  rnhodek
+ * *** empty log message ***
+ *
+ */
+#ifndef _strlib_h
+#define _strlib_h
+
+/* This is a dummy on Atari, because we use normal libc string functions */
+
+#endif  /* _strlib_h */
+
